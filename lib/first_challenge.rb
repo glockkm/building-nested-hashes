5ba2b8b4588c198551a41fcd2fc78efa1446bba3
Kimberly <kmglock@mail.lipscomb.edu>
2020-12-17 21:31:58 -0600

    Done.

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,3 +1,6 @@
 def first_challenge
-   epic_tragedy = {}
+   epic_tragedy = { # variable epic_tragedy
+      :montague => {}, # key which is a symbol because of :
+      :capulet => {}   # key
+    }
 end
--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,11 +1,18 @@
-def second_challenge
-  epic_tragedy = {
-    :montague => {
-      #your key/value pairs here!
-      }, 
-    :capulet => {
-      #your key/value pairs here!
-      }}
+# second level of our hash—the collection of data that constitutes the value hashes of each family name key
 
-  
+def second_challenge
+epic_tragedy = {
+   :montague => {
+      :patriarch => {}, # keys in montague hash
+      :matriarch => {}, # empty hashes
+      :hero => {},
+      :hero_friends => [] # empty array
+   },
+   :capulet => {
+      :patriarch => {}, # keys in capulet hash
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+   }
+}
 end
